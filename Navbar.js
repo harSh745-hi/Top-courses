6592d9a903bebb50c4f4c4ea81e87789673a63f3
harSh745-hi <harshpandharpatte@gmail.com>
2025-07-14 22:17:54 +0530

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,14 @@
+#Component "Navbar"
+import React from "react";
+
+const Navbar = () => 
+{
+     
+  return(
+    <div className="bg-bgDark  h-[65px] text-white text-center text-2xl font-extrabold py-4">
+      Top Courses
+    </div>
+  );
+
+}
+export default Navbar
