eb80a93343871c9ae9e7658614d25477d553aa51
harSh745-hi <harshpandharpatte@gmail.com>
2025-07-14 22:18:36 +0530

    Create Spinner.js

---
+++ Spinner.js
@@ -0,0 +1,19 @@
+#Component "Spinner"
+import React from 'react'
+import './Spinner.css';
+
+
+const Spinner = ()=>
+{
+    return (
+    <div>
+        <div className="spinner"> </div>
+        <p>Loading.. </p>
+       
+    </div>
+
+    )
+
+}
+
+export default Spinner 
